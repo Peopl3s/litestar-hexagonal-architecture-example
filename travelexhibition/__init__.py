54dd59921303a07672dd992567f3f553b6e84d7f
Мельников Максим <m.melnikov@idaproject.com>
2025-10-23 09:01:42 +0300

    LHE-3/add import linter

---
+++ travelexhibition/__init__.py
@@ -0,0 +1,8 @@
+"""
+Travel Exhibition - Hexagonal Architecture Example
+
+This package demonstrates hexagonal architecture (ports and adapters pattern)
+with Litestar framework.
+"""
+
+__version__ = "0.1.0"
