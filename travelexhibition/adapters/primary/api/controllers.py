b42228a491afcd7231d915efe4536ec1443ea025
Мельников Максим <m.melnikov@idaproject.com>
2025-11-13 07:35:11 +0300

    LHE-7/refactor

--- travelexhibition/adapters/primary/api/controllers.py
+++ travelexhibition/adapters/primary/api/controllers.py
@@ -3,17 +3,37 @@ from typing import Annotated
 
 from dishka.integrations.base import FromDishka as Depends
 from dishka.integrations.litestar import inject
-from litestar import Controller, route, HttpMethod
+from litestar import Controller, route, HttpMethod, Response
 from litestar.params import Parameter
+from litestar.status_codes import HTTP_400_BAD_REQUEST
 
 from travelexhibition.adapters.primary.api.schemas import ArtifactResponseSchema
+from travelexhibition.adapters.secondary.exceptions import SQLAlchemyReaderError, DataMapperError
 from travelexhibition.core.services import GetArtifactUseCase
 
 
 class ArtifactController(Controller):
     path = "/artifact"
 
-    @route(http_method=HttpMethod.GET, path="/{artifact_id:uuid}", response_model=ArtifactResponseSchema)
+    @route(
+        http_method=HttpMethod.GET,
+        path="/{artifact_id:uuid}",
+        response_model=ArtifactResponseSchema,
+        summary="Get Artifact by ID",
+        description="Fetches a specific artifact using its unique identifier (UUID).",
+        name="artifact:get_by_id",
+        tags=["Artifacts"],
+        exception_handlers={
+            SQLAlchemyReaderError: lambda request, exc: Response(
+                content={"error": "Bad request", "detail": str(exc)},
+                status_code=HTTP_400_BAD_REQUEST,
+            ),
+            DataMapperError: lambda request, exc: Response(
+                content={"error": "Bad request", "detail": str(exc)},
+                status_code=HTTP_400_BAD_REQUEST,
+            ),
+        }
+    )
     @inject
     async def get_artifact(
             self,
